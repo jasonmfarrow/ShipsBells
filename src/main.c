185a177ff1da7a3607670593a549baf557ba93ba
Jason M Farrow <jasonmfarrow@hotmail.com>
2015-01-08 14:07:12 +0000

    Version 1.0 for release

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <pebble.h>
 #include <time.h>  
+#include <stdlib.h>
+#include <stdio.h>
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
 #define KEY_LOCATION 2
@@ -12,10 +14,11 @@
 // Configurable options
 bool half_hour_bells = true;
 bool quarter_hour_bells = false;
-static int hour_bells_from = 8;
-static int hour_bells_to = 22;
+int hour_bells_from = 8;
+// int hour_bells_f = 8;
+int hour_bells_to = 22;
+// int hour_bells_t = 22;
 bool black_on_white = false;
-// APP_LOG(APP_LOG_LEVEL_INFO, "Bells From: %d", (int)hour_bells_from);
 
 // windows and layers  
 static Window *s_main_window;
@@ -80,50 +83,11 @@ const VibePattern QUARTER_HOUR_VIBE_PATTERN = {
     .num_segments = 6
 };
 
-static void vibrate_bells() {
-  // Get a tm structure
-  time_t now = time(NULL);
-  struct tm *tick_time = localtime(&now);
-  int hournow = tick_time->tm_hour;
-  // Deal with Whole Hours
-  if(tick_time->tm_min == 0) {
-    if(hournow == 0 || hournow == 4 || hournow == 8 || hournow == 12 || hournow == 16 || hournow == 20) {
-      vibes_enqueue_custom_pattern(EIGHT_BELLS);
-    }
-    if(hournow == 1 || hournow == 5 || hournow == 9 || hournow == 13 || hournow == 17 || hournow == 21) {
-      vibes_enqueue_custom_pattern(TWO_BELLS);
-    }
-    if(hournow == 2 || hournow == 6 || hournow == 10 || hournow == 14 || hournow == 18 || hournow == 22) {
-      vibes_enqueue_custom_pattern(FOUR_BELLS);
-    }
-    if(hournow == 4 || hournow == 7 || hournow == 11 || hournow == 15 || hournow == 19 || hournow == 23) {
-      vibes_enqueue_custom_pattern(SIX_BELLS);
-    }
-    // now deal with half hours
-  } else if((tick_time->tm_min % VIBE_INTERVAL_IN_MINUTES) == 0 && half_hour_bells) {
-    if(hournow == 0 || hournow == 4 || hournow == 8 || hournow == 12 || hournow == 16 || hournow == 20) {
-      vibes_enqueue_custom_pattern(ONE_BELL);
-    }
-    if(hournow == 1 || hournow == 5 || hournow == 9 || hournow == 13 || hournow == 17 || hournow == 21) {
-      vibes_enqueue_custom_pattern(THREE_BELLS);
-    }
-    if(hournow == 2 || hournow == 6 || hournow == 10 || hournow == 14 || hournow == 18 || hournow == 22) {
-      vibes_enqueue_custom_pattern(FIVE_BELLS);
-    }
-    if(hournow == 4 || hournow == 7 || hournow == 11 || hournow == 15 || hournow == 19 || hournow == 23) {
-      vibes_enqueue_custom_pattern(SEVEN_BELLS);
-    }
-    // now deal with quarter hours
-  } else if ((tick_time->tm_min % VIBE_SUBINTERVAL_IN_MINUTES) == 0 && quarter_hour_bells) {
-    vibes_enqueue_custom_pattern(QUARTER_HOUR_VIBE_PATTERN);
-  }
-}
-
 static void update_time() {
-  // Get a tm structure
+  // Get a tm time structure
   time_t now = time(NULL);
   struct tm *tick_time = localtime(&now);
-  // Create a buffer
+  // Create a clock text buffer
   static char buffer[] = "00:00";
   //Write current hours and minutes
   if(clock_is_24h_style() == true) {
@@ -134,15 +98,48 @@ static void update_time() {
     strftime(buffer, sizeof("00:00"), "I:%M", tick_time);
   }
   // Are we within the From and To hours?
-  if (tick_time->tm_hour >= hour_bells_from && tick_time->tm_hour < hour_bells_to) {
-    vibrate_bells();
+  int hournow = tick_time->tm_hour;
+  if (hournow >= hour_bells_from && hournow < hour_bells_to) {
+    // Deal with Whole Hours
+    if(tick_time->tm_min == 0) {
+      if(hournow == 0 || hournow == 4 || hournow == 8 || hournow == 12 || hournow == 16 || hournow == 20) {
+        vibes_enqueue_custom_pattern(EIGHT_BELLS);
+      }
+      if(hournow == 1 || hournow == 5 || hournow == 9 || hournow == 13 || hournow == 17 || hournow == 21) {
+        vibes_enqueue_custom_pattern(TWO_BELLS);
+      }
+      if(hournow == 2 || hournow == 6 || hournow == 10 || hournow == 14 || hournow == 18 || hournow == 22) {
+        vibes_enqueue_custom_pattern(FOUR_BELLS);
+      }
+      if(hournow == 4 || hournow == 7 || hournow == 11 || hournow == 15 || hournow == 19 || hournow == 23) {
+        vibes_enqueue_custom_pattern(SIX_BELLS);
+      }
+    // now deal with half hours
+    } else if((tick_time->tm_min % VIBE_INTERVAL_IN_MINUTES) == 0 && half_hour_bells) {
+      if(hournow == 0 || hournow == 4 || hournow == 8 || hournow == 12 || hournow == 16 || hournow == 20) {
+        vibes_enqueue_custom_pattern(ONE_BELL);
+      }
+      if(hournow == 1 || hournow == 5 || hournow == 9 || hournow == 13 || hournow == 17 || hournow == 21) {
+        vibes_enqueue_custom_pattern(THREE_BELLS);
+      }
+      if(hournow == 2 || hournow == 6 || hournow == 10 || hournow == 14 || hournow == 18 || hournow == 22) {
+        vibes_enqueue_custom_pattern(FIVE_BELLS);
+      }
+      if(hournow == 4 || hournow == 7 || hournow == 11 || hournow == 15 || hournow == 19 || hournow == 23) {
+        vibes_enqueue_custom_pattern(SEVEN_BELLS);
+      }
+    // now deal with quarter hours
+    } else if ((tick_time->tm_min % VIBE_SUBINTERVAL_IN_MINUTES) == 0 && quarter_hour_bells) {
+      vibes_enqueue_custom_pattern(QUARTER_HOUR_VIBE_PATTERN);
+    }
   }
-  // Display this time
+  // Complete the time layer
   text_layer_set_text(s_time_layer, buffer);
-  // Setup the date
+  // Create the date layer
   static char date_buffer[18];
   snprintf(date_buffer, sizeof(date_buffer), "%s %d %s %d", wday_name[tick_time->tm_wday], tick_time->tm_mday, mon_name[tick_time->tm_mon], 1900 + tick_time->tm_year);
   text_layer_set_text(s_date_layer, date_buffer);
+
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -158,40 +155,41 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     // Send the message!
     app_message_outbox_send();
   }
-  static char s_battery_buffer[16];
-  BatteryChargeState charge_state = battery_state_service_peek();
-  if (charge_state.is_charging) {
-    snprintf(s_battery_buffer, sizeof(s_battery_buffer), "Bat: chrg");
-  } else {
-    snprintf(s_battery_buffer, sizeof(s_battery_buffer), "Bat: %d%%", charge_state.charge_percent);
-  }
-  text_layer_set_text(s_battery_layer, s_battery_buffer);
+  // Create or update the battery layer every minute
+  if(tick_time->tm_min % 1 == 0){
+    static char s_battery_buffer[16];
+    BatteryChargeState charge_state = battery_state_service_peek();
+    if (charge_state.is_charging) {
+      snprintf(s_battery_buffer, sizeof(s_battery_buffer), "Bat: chrg");
+    } else {
+      snprintf(s_battery_buffer, sizeof(s_battery_buffer), "Bat: %d%%", charge_state.charge_percent);
+    }
+    text_layer_set_text(s_battery_layer, s_battery_buffer);
+  }  
 }
 
 static void main_window_load(Window *window) {
   //Check for saved options
   bool black_on_white = persist_read_bool(BLACK_ON_WHITE);
-  bool half_hour_bells = persist_read_bool(HALF_HOUR_BELLS);
-  bool quarter_hour_bells = persist_read_bool(QUARTER_HOUR_BELLS);
-  hour_bells_from = persist_read_int(HOUR_BELLS_FROM);
-  hour_bells_to = persist_read_int(HOUR_BELLS_TO);
-  // Create GBitmap, then set to created BitmapLayer
-  if(black_on_white) {
-      s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SHIPS_BELLS_BG);
-  } else {
-    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SHIPS_BELLS_BGB);
-  }
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  // Select the background picture (bitmap)
+  if(black_on_white) {
+    bitmap_layer_set_background_color(s_background_layer, GColorWhite);
+    // s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SHIPS_BELLS_BG);
+  } else {
+    bitmap_layer_set_background_color(s_background_layer, GColorBlack);
+    // s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SHIPS_BELLS_BGB);
+  }
+  // bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
-  // Create Display TextLayers
+  // Create content TextLayers
+  s_battery_layer = text_layer_create(GRect(72,0,72,18));
   s_time_layer = text_layer_create(GRect(0,12,144,50));
   s_weather_layer = text_layer_create(GRect(0,60,144,30));
-  s_half_layer = text_layer_create(GRect(0,145,71,25));
-  s_quarter_layer = text_layer_create(GRect(72,145,72,25));
-  s_battery_layer = text_layer_create(GRect(72,0,72,18));
   s_location_layer = text_layer_create(GRect(0,87,144,23));
   s_date_layer = text_layer_create(GRect(0,112,144,30));
+  s_half_layer = text_layer_create(GRect(0,145,71,23));
+  s_quarter_layer = text_layer_create(GRect(72,145,72,23));
   // Set text layer backgrounds
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_background_color(s_weather_layer, GColorClear);
@@ -239,22 +237,7 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   text_layer_set_text(s_weather_layer, "Loading...");
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
-  static char half_buffer[10];
-  static char qtr_buffer[10];
-  APP_LOG(APP_LOG_LEVEL_INFO, "Bells From: %d", (int)hour_bells_from);
-  APP_LOG(APP_LOG_LEVEL_INFO, "Bells To: %d", (int)hour_bells_to);
-  if(half_hour_bells) {
-    snprintf(half_buffer, sizeof(half_buffer), "H:On %d", (int)hour_bells_from);
-  } else {
-    snprintf(half_buffer, sizeof(half_buffer), "H:Off %d", (int)hour_bells_from);
-  }
-  if(quarter_hour_bells) {
-    snprintf(qtr_buffer, sizeof(qtr_buffer), "Q:On %d", (int)hour_bells_to);
-  } else {
-    snprintf(qtr_buffer, sizeof(qtr_buffer), "Q:Off %d", (int)hour_bells_to);
-  }
-  text_layer_set_text(s_half_layer, half_buffer);
-  text_layer_set_text(s_quarter_layer, qtr_buffer);
+
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_half_layer));  
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_quarter_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_battery_layer));
@@ -283,11 +266,16 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   static char conditions_buffer[32];
   static char location_buffer[16];
   static char weather_layer_buffer[32]; 
+  bool half_hour_bells = persist_read_bool(HALF_HOUR_BELLS);
+  bool quarter_hour_bells = persist_read_bool(QUARTER_HOUR_BELLS);
+  int hour_bells_from = persist_read_int(HOUR_BELLS_FROM); 
+  int hour_bells_to = persist_read_int(HOUR_BELLS_TO); 
+  bool black_on_white = persist_read_bool(BLACK_ON_WHITE);
   // Read first item
   Tuple *t = dict_read_first(iterator);
   // For all items
   while(t != NULL) {
-    APP_LOG(APP_LOG_LEVEL_INFO, "Value: %s", t->value->cstring);
+    // APP_LOG(APP_LOG_LEVEL_INFO, "Returned Key Value: %s", t->value->cstring);
     // Which key was received?
     switch(t->key) {
     case KEY_TEMPERATURE:
@@ -329,11 +317,16 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
       }
       break;
     case HOUR_BELLS_FROM:
-      APP_LOG(APP_LOG_LEVEL_INFO, "Bells From: %i", (int)t->value->int8);
-      persist_write_int(HOUR_BELLS_FROM, (int)t->value->int8);
+      // APP_LOG(APP_LOG_LEVEL_INFO, "Hour Bells From key value: %s", t->value->cstring);
+      hour_bells_from = atol(t->value->cstring);
+      persist_write_int(HOUR_BELLS_FROM, hour_bells_from);
+      // APP_LOG(APP_LOG_LEVEL_INFO, "Hour Bells From var value: %d", hour_bells_from);
       break;
     case HOUR_BELLS_TO:
-      persist_write_int(HOUR_BELLS_TO, (int)t->value->int8);
+      // APP_LOG(APP_LOG_LEVEL_INFO, "Hour Bells To key value: %s", t->value->cstring);
+      hour_bells_to = atol(t->value->cstring);
+      persist_write_int(HOUR_BELLS_TO, hour_bells_to);
+      // APP_LOG(APP_LOG_LEVEL_INFO, "Hour Bells to var value: %d", hour_bells_to);
       break;  
     default:
       APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
@@ -343,10 +336,49 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     t = dict_read_next(iterator);
   }
   // Assemble full string and display
+  static char half_buffer[10];
+  static char qtr_buffer[10];
+  // APP_LOG(APP_LOG_LEVEL_INFO, "Bells From: %d", hour_bells_from);
+  // APP_LOG(APP_LOG_LEVEL_INFO, "Bells To: %d", hour_bells_to);
+  // APP_LOG(APP_LOG_LEVEL_INFO, "Bells F: %d", hour_bells_f);
+  // APP_LOG(APP_LOG_LEVEL_INFO, "Bells T: %d", hour_bells_t);
+  if(half_hour_bells) {
+    snprintf(half_buffer, sizeof(half_buffer), "H:On %d", hour_bells_from);
+  } else {
+    snprintf(half_buffer, sizeof(half_buffer), "H:Off %d", hour_bells_from);
+  }
+  if(quarter_hour_bells) {
+    snprintf(qtr_buffer, sizeof(qtr_buffer), "Q:On %d", hour_bells_to);
+  } else {
+    snprintf(qtr_buffer, sizeof(qtr_buffer), "Q:Off %d", hour_bells_to);
+  }
+  text_layer_set_text(s_half_layer, half_buffer);
+  text_layer_set_text(s_quarter_layer, qtr_buffer);
   snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
   text_layer_set_text(s_weather_layer, weather_layer_buffer);
   text_layer_set_text(s_location_layer, location_buffer);
-  APP_LOG(APP_LOG_LEVEL_INFO, "Weather: %s,%s %s", temperature_buffer, conditions_buffer, location_buffer );
+  // APP_LOG(APP_LOG_LEVEL_INFO, "Weather: %s,%s %s", temperature_buffer, conditions_buffer, location_buffer );
+  if(black_on_white) {
+    bitmap_layer_set_background_color(s_background_layer, GColorWhite);
+    text_layer_set_text_color(s_time_layer, GColorBlack);
+    text_layer_set_text_color(s_weather_layer, GColorBlack);
+    text_layer_set_text_color(s_half_layer, GColorBlack);
+    text_layer_set_text_color(s_quarter_layer, GColorBlack);
+    text_layer_set_text_color(s_battery_layer, GColorBlack);
+    text_layer_set_text_color(s_location_layer, GColorBlack);
+    text_layer_set_text_color(s_date_layer, GColorBlack);
+    // s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SHIPS_BELLS_BG);
+  } else {
+    bitmap_layer_set_background_color(s_background_layer, GColorBlack);
+    text_layer_set_text_color(s_time_layer, GColorWhite);
+    text_layer_set_text_color(s_weather_layer, GColorWhite);
+    text_layer_set_text_color(s_half_layer, GColorWhite);
+    text_layer_set_text_color(s_quarter_layer, GColorWhite);
+    text_layer_set_text_color(s_battery_layer, GColorWhite);
+    text_layer_set_text_color(s_location_layer, GColorWhite);
+    text_layer_set_text_color(s_date_layer, GColorWhite);
+    // s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SHIPS_BELLS_BGB);
+  }
 } 
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
